b73e5ceb6f63c422cef6e56fbbe02d46ef45e5a4
mpetric <matt.petric@gmail.com>
2014-12-16 01:13:00 -0500

    about page 8x4 and footer updated

--- README.md
+++ README.md
@@ -1,4 +1,71 @@
-auctioning-detroit
+Auctioning Detroit
 ==================
 
-Map of properties sold through the Detroit Land Bank Authority online auction program
+<p>The City of Detroit, through the <strong>Detroit Land Bank
+Authority (DLBA)</strong>, has been auctioning homes since May 2014
+with the hopes of attracting new residents to the city's
+neighborhoods. The auctions come with strict rules. Detroit isn't
+in the market for investors looking for a quick buck and wants to
+prevent the homes from having the same fate as many of those
+auctioned through Wayne County's tax-foreclosure auctions, an
+auction system for which <a href=
+"http://michiganradio.org/post/wayne-county-sues-collect-back-taxes-properties-it-once-sold-tax-foreclosure-auctions"
+target="_blank">78 percent</a> of homes have fallen back into tax
+deliquency. Winning bidders in the DLBA auctions must agree to
+rehab the homes and establish occupancy, and Detroit follows up on
+the promise. Within 30 days of an auction closing, the buyer must
+produce a signed contract for rehabilitation/renovation or
+demonstrate the skill to carry out the rehab by his/herself. Within
+six months, the house must be rehabbed and occupied. Failure to
+meet these milestones results in the forfeiture of the property
+back to the Land Bank.</p>
+
+<p>Detroit has now auctioned a few hundred of these properties, and
+every day auctions a couple more. This website maps the results of
+those auctions and adds visualization tools to help the user better
+contextualize closing prices throughout the city. Data is updated
+through the end of November. I completed this project as part of a
+<a href=
+"https://www.pratt.edu/academics/continuing-education-and-professional/pro-certificate-programs/certificate-program-in-gis-and-design/%20target=">
+GIS and Design Certificate Program</a> at Pratt Institute.</p>
+
+<h3>About the Data</h3><img alt="Auction listings" src=
+"images/detroit_auctions.png">
+
+<p>DBLA's <a href=
+"http://auctions.buildingdetroit.org/Home/SoldHouses" target=
+"_blank">Past Auctions</a> website serves as the basis for
+"Auctioning Detroit." I scraped the website using the <a href=
+"http://scrapy.org/" target="_blank">Scrapy</a> framework for
+Python. <a href=
+"https://github.com/mpetric/auctioning-detroit/blob/master/scrapy/auctions/auctions/spiders/auction_spider.py"
+target="_blank">Code</a> for my spider, as well as all other code
+and data for this site, can be <a href=
+"https://github.com/mpetric/auctioning-detroit" target=
+"_blank">found on GitHub.</a> In addition, I supplemented the data
+from DBLA with lot and neighborhood information from a Detroit
+parcel's shapefile that can be downloaded <a href=
+"https://github.com/mpetric/auctioning-detroit/tree/master/data/detroit_polygons"
+target="_blank">here.</a> Finally, the neighborhood hover outlines
+came from a shapefile of Detroit's master plan neighborhoods from
+the City's <a href=
+"http://www.detroitmi.gov/DepartmentsandAgencies/PlanningDevelopmentDepartment/Planning/InformationServiceandMapping/CommunityInformationandMapping/AdvancedMaps/DownloadGISFiles.aspx"
+target="_blank">GIS Portal.</a></p>
+
+<h3>About the Site Design</h3>
+
+<p>The website is built using the <a href=
+"http://getbootstrap.com/">Bootstrap</a> front-end framework, which
+allows for relatively painless mobile-first responsive design.</p>
+
+<p>The map features are achieved using the <a href=
+"http://leafletjs.com/" target="_blank">Leaflet</a> JavaScript
+library, originally developed by Vladimir Agafonkin. Much of the
+site interactivity is accomplished via <a href="http://jquery.com/"
+target="_blank">jQuery</a>. The address search feature is built
+using the <a href="https://github.com/smeijer/L.GeoSearch" target=
+"_blank">GeoSearch</a> plugin by Stephan Meijer. The web site uses
+<a href="http://josscrowcroft.github.io/accounting.js/" target=
+"_blank">account.js</a> to easily format numbers as currency and
+other bits of code I picked up here and there on websites like
+Stack Overflow.</p>
--- js/script.js
+++ js/script.js
@@ -1,84 +1,58 @@
 //Hide footer on small screens
-
 if ($(window).width() < 1200) {
     $('#footer').css("display", "none");
 }
-
-
 // Set location of points geoJson
-
 var geoJsonFile = './js/auction_points.geojson';
-
-
 // Set up map and turn off default zoom control
-
 var map = L.map('map', {
     zoomControl: false
-})
-
-    .setView([42.371427, -83.045754], 12);
-
+}).setView([42.371427, -83.045754], 12);
 // Add zoom control to top right of screen
-
 map.addControl(L.control.zoom({
-    position: 'topright'
-}))
-
-
-// Create variable for basemap
-
+        position: 'topright'
+    }))
+    // Create variable for basemap
 var darkGreyColor = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
-
 // Add basemap to map
-
 L.tileLayer(darkGreyColor, {
     maxZoom: 18
 }).addTo(map);
 map.attributionControl.setPrefix('');
-
 // Add address search using geosearch plugin
-
 new L.Control.GeoSearch({
     provider: new L.GeoSearch.Provider.Google()
 }).addTo(map);
-
 // Tell leaflet location of image folder for Markers (since locally hosted)
-
 L.Icon.Default.imagePath = 'images/';
-
 // Load external geoJson and style markers for points according to style function
-
-$.getJSON(geoJsonFile, function (data) {
+$.getJSON(geoJsonFile, function(data) {
     var geojsonLayer = L.geoJson(data.features, {
         onEachFeature: makeMarkers,
-        pointToLayer: function (feature, latlng) {
+        pointToLayer: function(feature, latlng) {
             return L.circleMarker(latlng, style(feature) //style(feature) loads style from function above
             );
         }
     }).addTo(map);
 });
-
 // Create Info Control for Neighborhood Layer
 var info = L.control({
     position: 'bottomright'
 });
-
-info.onAdd = function (map) {
+info.onAdd = function(map) {
     this._div = L.DomUtil.create('div', 'info');
     this.update();
     return this._div;
 };
-
-info.update = function (props) {
-    this._div.innerHTML = '<h5>Neighborhood:</h5>' +  (props ?
-        '<b><div id="offset">' + props.NHOOD + '</b></div>' + '<h5>Auctions:</h5><b><div id="offset">' +  props.Count_ + ' homes</b></div>'
-        : '<div id="offset"><b>Hover over a neighborhood</b></div>');
+info.update = function(props) {
+    this._div.innerHTML = '<h5>Neighborhood:</h5>' + (props ?
+        '<b><div id="offset">' + props.NHOOD + '</b></div>' +
+        '<h5>Auctions:</h5><b><div id="offset">' + props.Count_ +
+        ' homes</b></div>' :
+        '<div id="offset"><b>Hover over a neighborhood</b></div>');
 };
-
 info.addTo(map);
-
 // Load external geojson file for neighborhood polygons, style, and turn on mouseover and zoom effects
-
 var myStyle = {
     "color": "rgb(125,125,125)",
     "weight": 2,
@@ -89,7 +63,6 @@ var myStyle = {
 
 function highlightFeature(e) {
     var layer = e.target;
-
     layer.setStyle({
         weight: 5,
         color: 'rgb(150,150,150)',
@@ -100,7 +73,6 @@ function highlightFeature(e) {
     layer.bringToBack();
     info.update(layer.feature.properties);
 }
-
 var geojson;
 
 function resetHighlight(e) {
@@ -119,172 +91,164 @@ function onEachFeature(feature, layer) {
         click: zoomToFeature
     });
 }
-
 geojson = L.geoJson(neighborhoods, {
-            style: myStyle,
-            onEachFeature: onEachFeature
-        }).addTo(map);
-
+    style: myStyle,
+    onEachFeature: onEachFeature
+}).addTo(map);
 // Return color and radius of point marker based on auction close price
-
 function getColor(x) {
-    return x > 50000 ? "#005a32" : x > 25000 ? "#238b45" : x > 10000 ? "#41ab5d" : x > 5000 ? "#74c476" : x > 1000 ? "#a1d99b" :
-        "#c7e9c0";
+    return x > 50000 ? "#005a32" : x > 25000 ? "#238b45" : x > 10000 ? "#41ab5d" :
+        x > 5000 ? "#74c476" : x > 1000 ? "#a1d99b" : "#c7e9c0";
 }
 
 function getFontColor(z) {
-    return z > 10000.1 ? "white" :
-        "#191919";
+    return z > 10000.1 ? "white" : "#191919";
 }
 
 function getRadius(r) {
-    return r > 50000 ? "12.5" : r > 25000 ? "11" : r > 10000 ? "10" : r > 5000 ? "9" : r > 1000 ? "8.5" :
-        "8";
-}
-
-// Setup style function which passes elements through getColor and getRadius functions
+        return r > 50000 ? "12.5" : r > 25000 ? "11" : r > 10000 ? "10" : r > 5000 ?
+            "9" : r > 1000 ? "8.5" : "8";
+    }
+    // Setup style function which passes elements through getColor and getRadius functions
 
 function style(feature) {
-    return {
-        "fillColor": getColor(feature.properties.price),
+        return {
+            "fillColor": getColor(feature.properties.price),
             "opacity": 1,
             "radius": getRadius(feature.properties.price),
             "color": "#000",
             "weight": 1,
             "fillOpacity": 0.8
-    };
-}
-
-
-//Create legend based off of getColor function
-
+        };
+    }
+    //Create legend based off of getColor function
 var legend = L.control({
     position: 'bottomleft'
 });
-legend.onAdd = function (map) {
-
+legend.onAdd = function(map) {
     var div = L.DomUtil.create('div', 'legend'),
         grades = [0, 1000, 5000, 10000, 25000, 50000],
         labels = ['<strong><span id="legendTitle"> Sale Price </span></strong>'],
         from, to;
-
     for (var i = 0; i < grades.length; i++) {
         from = grades[i];
         to = grades[i + 1] - 0;
-
-        labels.push(
-            '<i style="background:' + getColor(from + 1) + '"></i> $' + from + (to ? '&ndash;$' + to : '+'));
+        labels.push('<i style="background:' + getColor(from + 1) + '"></i> $' + from +
+            (to ? '&ndash;$' + to : '+'));
     }
     div.innerHTML = labels.join('<br>');
     return div;
 };
 legend.addTo(map);
-
-
 //Create function to format number with thousands seperator for square feet
-
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
-}
-
-
-//Setup on click and mouseover actions
+        return x.toString().replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
+    }
+    //Setup on click and mouseover actions
 
 function makeMarkers(feature, layer) {
-    var thisFeature = feature.properties;
-    //console.log(feature);
-
-    layer.on("click", function (e) {
-        var priceFormat = accounting.formatMoney(thisFeature.price, "$", 0, ",", "."); //accesses accounting.js to format into currency
-        var priceFormatNGBH = accounting.formatMoney(thisFeature.sale_summaries_NGBR_AVG, "$", 0, ",", ".");
-        var squareFeetFormat = numberWithCommas(thisFeature.squareFeet); //format square feet with thousands seperator
-        //begin formatting for date
-        var year = thisFeature.saleDate.substring(0, 4);
-        var month = thisFeature.saleDate.substring(5, 7);
-        var day = thisFeature.saleDate.substring(8, 10);
-        var name = month
-        if (name == "01") {
-            var monthName = "January";
-        }
-        if (name == "02") {
-            var monthName = "February";
-        }
-        if (name == "03") {
-            var monthName = "March";
-        }
-        if (name == "04") {
-            var monthName = "April";
-        }
-        if (name == "05") {
-            var monthName = "May";
-        }
-        if (name == "06") {
-            var monthName = "June";
-        }
-        if (name == "07") {
-            var monthName = "July";
-        }
-        if (name == "08") {
-            var monthName = "August";
-        }
-        if (name == "09") {
-            var monthName = "September";
-        }
-        if (name == "10") {
-            var monthName = "October";
-        }
-        if (name == "11") {
-            var monthName = "November";
-        }
-        if (name == "12") {
-            var monthName = "December";
-        }
-        var formattedDate = monthName + " " + day + ", " + year;
-        //end formatting for date
-        // map.panTo(new L.LatLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]));
-        $('#address').text(thisFeature.address)
-        var thisPropertyWidth = Math.log10(thisFeature.price)*Math.log10(thisFeature.price)*Math.log10(thisFeature.price)*4.6;
-        $('#thisProperty').text(thisFeature.address).css({"width" : thisPropertyWidth, "background-color" : getColor(thisFeature.price), "color" : getFontColor(thisFeature.price)})
-        $('#bedrooms').text(thisFeature.bedrooms)
-        $('#squareFeet').text(squareFeetFormat)
-        $('#price').text(priceFormat)
-        $('#priceChart').text(priceFormat)
-        $('#priceChartNGBH').text(priceFormatNGBH)
-        $('#bathrooms').text(thisFeature.bathrooms)
-        $('#saleDate').text(formattedDate)
-        $('#yearBuilt').text(thisFeature.YEARBUILT)
-        $('#full_url').attr("href", thisFeature.full_url)
-        $('#full_url_2').attr("href", thisFeature.full_url)
-        $('#housePhoto').attr("src", "./" + thisFeature.image)
-        $('#neighborhoodAvg').text(thisFeature.sale_summaries_NGBR_AVG)
-        $('#neighborhood').text(thisFeature.MPSUBSEC01)
-        var neighborhoodWidth = Math.log10(thisFeature.sale_summaries_NGBR_AVG)*Math.log10(thisFeature.sale_summaries_NGBR_AVG)*Math.log10(thisFeature.sale_summaries_NGBR_AVG)*4.6;
-        $('#neighborhoodName').text(thisFeature.MPSUBSEC01).css({"width" : neighborhoodWidth, "background-color" : getColor(thisFeature.sale_summaries_NGBR_AVG), "color" : getFontColor(thisFeature.sale_summaries_NGBR_AVG)})
-    });
-    layer.on({
-        mouseover: function over(e) {
-            layer.bindPopup(L.popup().setContent(thisFeature.address))
-            layer.openPopup()
-        },
-        mouseout: function out(e) {
-            layer.closePopup()
-        }
-    });
-}
-
-
-
-//Script for Footer
-$(document).ready(function () {
-    $('.open').click(function () {
+        var thisFeature = feature.properties;
+        //console.log(feature);
+        layer.on("click", function(e) {
+            var priceFormat = accounting.formatMoney(thisFeature.price, "$", 0, ",",
+                "."); //accesses accounting.js to format into currency
+            var priceFormatNGBH = accounting.formatMoney(thisFeature.sale_summaries_NGBR_AVG,
+                "$", 0, ",", ".");
+            var squareFeetFormat = numberWithCommas(thisFeature.squareFeet); //format square feet with thousands seperator
+            //begin formatting for date
+            var year = thisFeature.saleDate.substring(0, 4);
+            var month = thisFeature.saleDate.substring(5, 7);
+            var day = thisFeature.saleDate.substring(8, 10);
+            var name = month
+            if (name == "01") {
+                var monthName = "January";
+            }
+            if (name == "02") {
+                var monthName = "February";
+            }
+            if (name == "03") {
+                var monthName = "March";
+            }
+            if (name == "04") {
+                var monthName = "April";
+            }
+            if (name == "05") {
+                var monthName = "May";
+            }
+            if (name == "06") {
+                var monthName = "June";
+            }
+            if (name == "07") {
+                var monthName = "July";
+            }
+            if (name == "08") {
+                var monthName = "August";
+            }
+            if (name == "09") {
+                var monthName = "September";
+            }
+            if (name == "10") {
+                var monthName = "October";
+            }
+            if (name == "11") {
+                var monthName = "November";
+            }
+            if (name == "12") {
+                var monthName = "December";
+            }
+            var formattedDate = monthName + " " + day + ", " + year;
+            //end formatting for date
+            // map.panTo(new L.LatLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]));
+            $('#address').text(thisFeature.address)
+            var thisPropertyWidth = Math.log10(thisFeature.price) * Math.log10(
+                thisFeature.price) * Math.log10(thisFeature.price) * 3.68;
+            $('#thisProperty').text(thisFeature.address).css({
+                "width": thisPropertyWidth,
+                "background-color": getColor(thisFeature.price),
+                "color": getFontColor(thisFeature.price)
+            })
+            $('#bedrooms').text(thisFeature.bedrooms)
+            $('#squareFeet').text(squareFeetFormat)
+            $('#price').text(priceFormat)
+            $('#priceChart').text(priceFormat)
+            $('#priceChartNGBH').text(priceFormatNGBH)
+            $('#bathrooms').text(thisFeature.bathrooms)
+            $('#saleDate').text(formattedDate)
+            $('#yearBuilt').text(thisFeature.YEARBUILT)
+            $('#full_url').attr("href", thisFeature.full_url)
+            $('#full_url_2').attr("href", thisFeature.full_url)
+            $('#housePhoto').attr("src", "./" + thisFeature.image)
+            $('#neighborhoodAvg').text(thisFeature.sale_summaries_NGBR_AVG)
+            $('#neighborhood').text(thisFeature.MPSUBSEC01)
+            var neighborhoodWidth = Math.log10(thisFeature.sale_summaries_NGBR_AVG) *
+                Math.log10(thisFeature.sale_summaries_NGBR_AVG) * Math.log10(thisFeature.sale_summaries_NGBR_AVG) *
+                3.68;
+            $('#neighborhoodName').text(thisFeature.MPSUBSEC01).css({
+                "width": neighborhoodWidth,
+                "background-color": getColor(thisFeature.sale_summaries_NGBR_AVG),
+                "color": getFontColor(thisFeature.sale_summaries_NGBR_AVG)
+            })
+        });
+        layer.on({
+            mouseover: function over(e) {
+                layer.bindPopup(L.popup().setContent(thisFeature.address))
+                layer.openPopup()
+            },
+            mouseout: function out(e) {
+                layer.closePopup()
+            }
+        });
+    }
+    //Script for Footer
+$(document).ready(function() {
+    $('.open').click(function() {
         $('#footer').animate({
             bottom: 0
         });
         $(this).hide();
         $('.closeFooter').show();
     });
-
-    $('.closeFooter').click(function () {
+    $('.closeFooter').click(function() {
         $('#footer').animate({
             bottom: "-270px"
         });
@@ -292,21 +256,17 @@ $(document).ready(function () {
         $('.open').show();
     });
 });
-
-
 // Script for About Page
-$('#about').on('click',function(){
+$('#about').on('click', function() {
     $('#mask').fadeIn(250);
     $('.popup').fadeIn(250);
     $('.popupBackground').fadeIn(250);
 });
-
-$('#close').on('click',function(){
+$('#close').on('click', function() {
     $('.popup').fadeOut(250);
     $('.popupBackground').fadeOut(250);
     $('#mask').fadeOut(250);
 });
-
 //Turn off info window if small display; run function at end of script to override div info creation
 if ($(window).width() < 1000) {
     $('.info').css("display", "none");
